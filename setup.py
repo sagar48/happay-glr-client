5c17095f39d1b9e0f9ad2cde8f044c60b9491a85
Vidyasagar Goutam <vidyasagar.goutam@happay.in>
2019-08-22 13:30:53 +0530

    testing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Happay-GLR python client for backend module',
     url='git@github.com:sagar48/happay-glr-client.git',
     author='Vidyasagar Goutam',
-    author_email='vidyasagar.goutam@happay.in',
+    author_email='vidyasagargtm@gmail.com',
     license='unlicense',
     packages=['happay-glr-client'],
     zip_safe=False
